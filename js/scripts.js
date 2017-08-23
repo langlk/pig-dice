8b14754c975b6a2880ba40d8d703342d3f85cdc2
Kelsey Langlois and Linda Luu <kelsey@>
2017-08-23 09:28:26 -0700

    Add function to roll dice until player holds or rolls a one, and return turn score.

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,25 @@ function diceRoll(){
 
 function turn(){
   var roll = diceRoll();
-  console.log(roll);
-  if (roll === 1){
-    return 0;
+  var action = " ";
+  var total = 0;
+  while (roll !== 1){
+    total += roll;
+    action = promptPlayer(total);
+    if (action === "hold") {
+      return total;
+    } else {
+      roll = diceRoll();
+    }
   }
+  alert("You rolled a one!");
+  return 0;
 }
 
-
+function promptPlayer(score) {
+  var action = prompt("Your score is " + score + ". Roll or Hold?");
+  return action.toLowerCase();
+}
 
 $(document).ready(function(){
   console.log(turn());
